635fbae77a6c4f5aa18713ec4c3a44d085b10e4c
caleb <caleb@becomingmachinic.com>
2025-07-05 17:51:09 -0500

    initial attempt to get working on linux

--- src/rider/main/kotlin/run/RunState.kt
+++ src/rider/main/kotlin/run/RunState.kt
@@ -7,8 +7,8 @@ import com.intellij.execution.configurations.RunProfileState
 import com.intellij.execution.process.*
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.runners.ProgramRunner
-import com.intellij.ide.actions.searcheverywhere.evaluate
 import com.intellij.util.system.OS
+import com.intellij.openapi.util.Key
 import com.jetbrains.rd.util.lifetime.Lifetime
 import com.jetbrains.rider.debugger.DebuggerWorkerProcessHandler
 import com.jetbrains.rider.plugins.unity.run.configurations.UnityAttachProfileState
@@ -18,6 +18,7 @@ import java.io.File
 import java.io.FileOutputStream
 import java.nio.file.Files
 import kotlin.io.path.Path
+import com.intellij.execution.process.ProcessListener
 
 class RunState(
     private val rimworldLocation: String,
@@ -54,6 +55,14 @@ class RunState(
             "Doorstop/Mono.Cecil.dll",
             "Doorstop/Mono.CompilerServices.SymbolWriter.dll",
             "Doorstop/pdb2mdb.exe",
+        ),
+        OS.Linux to listOf(
+            ".doorstop_version",
+            "doorstop_config.ini",
+            "Doorstop/libdoorstop.so",
+            "Doorstop/0Harmony.dll",
+            "Doorstop/Mono.Cecil.dll",
+            "Doorstop/Mono.CompilerServices.SymbolWriter.dll"
         )
     )
     
@@ -83,6 +92,11 @@ class RunState(
                 QuickStartUtils.tearDown(saveFilePath)
                 removeDoorstep()
             }
+
+            override fun onTextAvailable(event: ProcessEvent, outputType: Key<*>) {
+                println("Handler Output: ${event.text}")
+                super.onTextAvailable(event, outputType)
+            }
         }
     }
 
@@ -98,20 +112,22 @@ class RunState(
             }
 
             file.createNewFile()
-
             val resourceStream = this.javaClass.getResourceAsStream("$basePath/$name") ?: return
+            println("Coping $basePath/$name to ${file.absolutePath}")
 
             val fileWriteStream = FileOutputStream(file)
             fileWriteStream.write(resourceStream.readAllBytes())
             fileWriteStream.close()
         }
 
+        println("Coping Doorstop files")
         currentResources.forEach {
             copyResource("/UnityDoorstop/${OS.CURRENT}/", it)
         }
 
         val harmonyDll = ScopeHelper.findInstalledHarmonyDll()
         if (harmonyDll != null) {
+            println("Coping $harmonyDll to ${rimworldDir}/Doorstop/0Harmony.dll")
             File(harmonyDll).copyTo(File("${rimworldDir}/Doorstop/0Harmony.dll"), true)
         }
     }
@@ -120,10 +136,12 @@ class RunState(
         val currentResources = resources[OS.CURRENT] ?: return
         Thread.sleep(50)
 
+        println("Removing Doorstop files")
         val rimworldDir = Path(rimworldLocation).parent.toFile()
 
         val removeResource = fun(name: String) {
             val file = File(rimworldDir, name)
+            println("Removing ${file.absolutePath}")
             file.delete()
         }
 
